cac6e8ca471dda263e8ec61df2faf1d29e758017
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2021-09-19 12:24:51 +0530

    prefinal

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,22 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/*/.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      outline: {
+        blue: '2px solid #0000ff',
+        gray: '2px solid #181818',
+      }
+
+    },
   },
   variants: {
     extend: {},
-    boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus','active'] ,
-    scale: ['responsive', 'hover', 'focus' ,'active'],
+    boxShadow: ['responsive', 'group-hover', 'focus-within', 'hover', 'focus', 'active'],
+    scale: ['responsive', 'hover', 'focus', 'active'],
     backgroundColor: ['responsive', 'dark', 'group-hover', 'focus-within', 'hover', 'focus'],
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
 }
